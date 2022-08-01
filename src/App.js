f5122bde58c450efcb062c6a5133bbd1ddc4a954
BoykoLena <boykolena15@gmail.com>
2022-08-01 11:36:46 +0300

    added some css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-center">Dictionary</h1>
+      <form className="text-center">
+        <input type="text"></input>
+        <input type="submit"></input>
+      </form>
+    </div>
+  );
 }
 
 export default App;
