fde508779fe9f7e71526899263ccd462c5c66140
BoykoLena <boykolena15@gmail.com>
2022-08-01 18:33:18 +0300

    created start page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center">Dictionary</h1>
-      <form className="text-center">
+      <h1 className="text-center">What word do you want to look up?</h1>
+      <form className="d-flex justify-content-center align-items-end">
         <input type="text"></input>
-        <input type="submit"></input>
+        <button>
+          <i class="fa-solid fa-arrow-right-long"></i>
+        </button>
       </form>
     </div>
   );
